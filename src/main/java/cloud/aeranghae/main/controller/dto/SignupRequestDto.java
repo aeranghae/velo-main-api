3a30ec296a6186a8e36d4d4c3ab672828a762957
oxxultus <a38027928@gmail.com>
2026-05-12 21:48:06 +0900

    패치 테스트

--- src/main/java/cloud/aeranghae/main/controller/dto/SignupRequestDto.java
+++ src/main/java/cloud/aeranghae/main/controller/dto/SignupRequestDto.java
@@ -1,10 +1,12 @@
 package cloud.aeranghae.main.controller.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class SignupRequestDto {
     private String nickname;
 }
