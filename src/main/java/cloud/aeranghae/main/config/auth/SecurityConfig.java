9ddf6dbb2ee2cdcd9532621a38fcdf05d752d7a9
oxxultus <a38027928@gmail.com>
2026-05-12 23:40:22 +0900

    Options를 위한 임시코드 추가

--- src/main/java/cloud/aeranghae/main/config/auth/SecurityConfig.java
+++ src/main/java/cloud/aeranghae/main/config/auth/SecurityConfig.java
@@ -66,7 +66,6 @@ public class SecurityConfig {
         // 일렉트론(리액트) 로컬 개발 서버 주소 명시적 허용
         //config.setAllowedOrigins(allowedOrigins);
         config.setAllowedOriginPatterns(List.of("*"));
-
         // 허용할 HTTP 메서드
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         // 허용할 헤더
