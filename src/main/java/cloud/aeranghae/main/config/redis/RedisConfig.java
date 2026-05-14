a55ed9132f8e07a8e1fcbbdae9daccac3a90f154
oxxultus <a38027928@gmail.com>
2026-05-14 16:24:24 +0900

    레디스 오브젝트 매핑 문제

--- src/main/java/cloud/aeranghae/main/config/redis/RedisConfig.java
+++ src/main/java/cloud/aeranghae/main/config/redis/RedisConfig.java
@@ -23,14 +23,11 @@ public class RedisConfig {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
 
-        // 1. 4.0 표준: 이제는 objectMapper를 반드시 인자로 넘겨줘야 합니다.
-        // 스프링이 자동으로 관리하는 objectMapper를 사용하면 설정이 공유되어 편리합니다.
+        // Spring Boot 4.0에서 제공하는 기본 ObjectMapper를 주입받아 사용합니다.
         GenericJacksonJsonRedisSerializer serializer = new GenericJacksonJsonRedisSerializer(objectMapper);
 
-        // 2. Key/Value 직렬화 설정
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(serializer);
-
         template.setHashKeySerializer(new StringRedisSerializer());
         template.setHashValueSerializer(serializer);
 
@@ -40,13 +37,12 @@ public class RedisConfig {
 
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory, ObjectMapper objectMapper) {
-        // 4.0 표준 GenericJacksonJsonRedisSerializer 사용
         GenericJacksonJsonRedisSerializer serializer = new GenericJacksonJsonRedisSerializer(objectMapper);
 
         RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
                 .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
                 .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer))
-                .entryTtl(Duration.ofMinutes(10)); // 기본 10분 동안 캐싱
+                .entryTtl(Duration.ofMinutes(10));
 
         return RedisCacheManager.builder(connectionFactory)
                 .cacheDefaults(config)
