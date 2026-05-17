fcde4ed018aa726659394e3e6351a381cceac8a1
oxxultus <a38027928@gmail.com>
2026-05-17 18:21:21 +0900

    ProjectNodeResponse 역직렬화 문제 해결

--- src/main/java/cloud/velo/main/controller/dto/ProjectNodeResponse.java
+++ src/main/java/cloud/velo/main/controller/dto/ProjectNodeResponse.java
@@ -1,9 +1,13 @@
 package cloud.velo.main.controller.dto;
 
 import cloud.velo.main.domain.ProjectNode;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(force = true)
+@AllArgsConstructor
 public class ProjectNodeResponse {
     private final String path;
     private final String type; // "DIR" 또는 "FILE"
